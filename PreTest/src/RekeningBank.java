27071e4f273861382d4681ea4f2ca020245b69c0
Sennn306 <adrenalinsyahrobby30@gmail.com>
2026-04-02 15:19:40 +0700

    Pengumpulan Pretest

---
+++ PreTest/src/RekeningBank.java
@@ -0,0 +1,19 @@
+public class RekeningBank {
+    private String nomorRekening;
+    private double saldo;
+
+    public RekeningBank(String nomorRekening, double saldo) {
+        this.nomorRekening = nomorRekening;
+        this.saldo = saldo;
+    }
+    public double getSaldo(){
+        return saldo;
+    }
+    public void setSaldo(double upSaldo){
+        if (upSaldo > 0) {
+            this.saldo = saldoUpdate;
+        } else {
+            System.out.println("Saldo tidak boleh negatif");
+        }
+    }
+}
